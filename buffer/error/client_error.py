6c3f45949d98d5fc9703b0f6fba4364e0173705e
Artem <artem.e.shelest@gmail.com>
2017-10-23 20:10:28 +0300

    Error message is passed as a parameter to Exception base class (#3)

--- buffer/error/client_error.py
+++ buffer/error/client_error.py
@@ -3,6 +3,6 @@ class ClientError(Exception):
     """ClientError is used when the API returns an error"""
 
     def __init__(self, message, code):
-        super(ClientError, self).__init__()
+        super(ClientError, self).__init__(message)
         self.message = message
         self.code = code
